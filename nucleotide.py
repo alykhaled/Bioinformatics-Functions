cbd4960ef3d340b1d9febc4a6ef40b2eb08d2633
Aly Khaled <alykhaled2001@live.com>
2023-06-24 14:14:20 +0300

    Fix

--- nucleotide.py
+++ nucleotide.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from frequentWord import frequentWords, betterFrequentWords, findClumps, frequencyTable,optimizedFindClumps
+from frequentWords import frequentWords, betterFrequentWords, findClumps, frequencyTable,optimizedFindClumps
 
 def reverese_complement(seq):
     """Return the reverse complement of the input sequence."""
@@ -77,6 +77,84 @@ def immediateNeighbors(pattern):
                 neighbors.append(neighbor)
     return neighbors
 
+def neighbors(pattern, d):
+    if d == 0:
+        return [pattern]
+    if len(pattern) == 1:
+        return ['A', 'C', 'G', 'T']
+    neighborhood = []
+    suffixNeighbors = neighbors(pattern[1:], d)
+    for text in suffixNeighbors:
+        if hammingDistance(pattern[1:], text) < d:
+            for nucleotide in ['A', 'C', 'G', 'T']:
+                neighborhood.append(nucleotide + text)
+        else:
+            neighborhood.append(pattern[0] + text)
+    return neighborhood
+
+def frequentWordsWithMismatches(genome, k, d):
+    frequentPatterns = []
+    neighborhoods = []
+    for i in range(len(genome) - k + 1):
+        neighborhoods.append(neighbors(genome[i:i+k], d))
+    neighborhoods = [item for sublist in neighborhoods for item in sublist]
+    neighborhoods = list(set(neighborhoods))
+    counts = []
+    for i in range(len(neighborhoods)):
+        pattern = neighborhoods[i]
+        counts.append(updatedFrequencyWords(genome, pattern, d))
+    maxCount = max(counts)
+    for i in range(len(neighborhoods)):
+        if counts[i] == maxCount:
+            frequentPatterns.append(neighborhoods[i])
+    return frequentPatterns
+
+def frequentWordsWithMismatchesAndReverseComplements(genome, k, d):
+    frequentPatterns = []
+    neighborhoods = []
+    genome = genome + reverese_complement(genome)
+    for i in range(len(genome) - k + 1):
+        neighborhoods.append(neighbors(genome[i:i+k], d))
+    neighborhoods = [item for sublist in neighborhoods for item in sublist]
+    neighborhoods = list(set(neighborhoods))
+    counts = []
+    for i in range(len(neighborhoods)):
+        pattern = neighborhoods[i]
+        counts.append(updatedFrequencyWords(genome, pattern, d))
+    maxCount = max(counts)
+    for i in range(len(neighborhoods)):
+        if counts[i] == maxCount:
+            frequentPatterns.append(neighborhoods[i])
+    return frequentPatterns
+
+def solveFrequentWordsWithMismatchesAndReverseComplements():
+    with open('data/dataset_9_10.txt') as f:
+        genome = f.readline().strip()
+        k, d = f.readline().strip().split()
+    k = int(k)
+    d = int(d)
+    frequentPatterns = frequentWordsWithMismatchesAndReverseComplements(genome, k, d)
+    for pattern in frequentPatterns:
+        print(pattern, end=' ')
+
+def solveFrequentWordsWithMismatches():
+    with open('data/dataset_9_9.txt') as f:
+        genome = f.readline().strip()
+        k, d = f.readline().strip().split()
+    k = int(k)
+    d = int(d)
+    frequentPatterns = frequentWordsWithMismatches(genome, k, d)
+    for pattern in frequentPatterns:
+        print(pattern, end=' ')
+
+def solveNeighbors():
+    with open('data/dataset_3014_4.txt') as f:
+        pattern = f.readline().strip()
+        d = int(f.readline().strip())
+    neighborsList = neighbors(pattern, d)
+    for neighbor in neighborsList:
+        print(neighbor, end=' ')
+
 def solveUpdatedFrequencyWords():
     with open('data/dataset_9_6.txt') as f:
         pattern = f.readline().strip()
@@ -114,6 +192,23 @@ def solveMinSkew():
     for pos in minSkews:
         print(pos, end=' ')
 
+def readFasta(filename):
+    with open(filename) as f:
+        genome = ''
+        for line in f:
+            if line[0] != '>':
+                genome += line.strip()
+    return genome
+
+def solveSalmonella():
+    genome = readFasta('data/Salmonella_enterica.txt')
+    minSkews = minSkew(genome)
+    minGenomes = genome[minSkews[0]:minSkews[0]+500]
+    print(len(minGenomes))
+    boxes = frequentWordsWithMismatchesAndReverseComplements(minGenomes, 9, 1)
+    for box in boxes:
+        print(box, end=' ')
+
 # solveMinSkew()
 # plotSkewFromFile()
 # solveFromFile()
@@ -122,4 +217,11 @@ def solveMinSkew():
 # solveApproximatePatternMatching()
 # print(updatedFrequencyWords('TTTAGAGCCTTCAGAGG', 'GAGG', 2))
 # solveUpdatedFrequencyWords()
-print(immediateNeighbors('ACG'))
+# print(immediateNeighbors('ACG'))
+# print(neighbors('ACG', 1))
+# solveNeighbors()
+# print(frequentWordsWithMismatches('ACGTTGCATGTCGCATGATGCATGAGAGCT', 4, 1))
+# solveFrequentWordsWithMismatches()
+# print(frequentWordsWithMismatchesAndReverseComplements('ACGTTGCATGTCGCATGATGCATGAGAGCT', 4, 1))
+# solveFrequentWordsWithMismatchesAndReverseComplements()
+solveSalmonella()
